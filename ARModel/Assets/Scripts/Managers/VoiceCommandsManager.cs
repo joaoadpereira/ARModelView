a757b81974a56c7e2e6c9f7a0c4bf6f9b8edcd1a
Joao Pereira <joaoadelino.pereira@gmail.com>
2025-03-31 11:58:39 +0100

    feat(unity): Add logic to VoiceCommandsManager

--- ARModel/Assets/Scripts/Managers/VoiceCommandsManager.cs
+++ ARModel/Assets/Scripts/Managers/VoiceCommandsManager.cs
@@ -4,6 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Android;
+using Whisper.Utils;
+using Whisper;
+using TMPro;
+using UnityEngine.UI;
+using Utils;
 
 namespace Managers 
 { 
@@ -15,8 +20,26 @@ namespace Managers
         #region fields
         private static VoiceCommandsManager instance;
 
+        private string recordText = "Click to speak commands";
+        private string stopRecordingText = "Stop recording";
+
+        private ButtonMenu buttonMenu;
+
         // handle speech Recognizion
-        
+        [SerializeField]
+        private WhisperManager whisper;
+        [SerializeField]
+        public MicrophoneRecord microphoneRecord;
+
+        [Header("UI that handles Speech")]
+        [SerializeField]
+        private Button button;
+        [SerializeField]
+        private TMP_Text buttonText;
+        [SerializeField]
+        private TMP_Text text;
+        [SerializeField]
+        private WhisperStream stream;
 
         #endregion
 
@@ -32,6 +55,8 @@ namespace Managers
 
         #endregion
 
+        #region private methods
+
         private void Awake()
         {
             // Set the current instance as the singleton or destroy case duplicated
@@ -43,11 +68,18 @@ namespace Managers
             {
                 instance = this;
             }
+
+            buttonMenu = button.GetComponent<ButtonMenu>();
+
+            if (buttonMenu == null)
+            {
+                Debug.LogError("Button Menu not found in speech button");
+            }
         }
 
-        #region private methods
+       
         // Start is called before the first frame update
-        void Start()
+        private async void Start()
         {
             // Check if permission is granted
             if (!Permission.HasUserAuthorizedPermission(Permission.Microphone))
@@ -56,7 +88,69 @@ namespace Managers
                 Permission.RequestUserPermission(Permission.Microphone);
             }
 
-           
+            // set whisper and listners 
+            stream = await whisper.CreateStream(microphoneRecord);
+            stream.OnResultUpdated += OnResult;
+            stream.OnSegmentUpdated += OnSegmentUpdated;
+            stream.OnSegmentFinished += OnSegmentFinished;
+            stream.OnStreamFinished += OnFinished;
+
+            microphoneRecord.OnRecordStop += OnRecordStop;
+            button.onClick.AddListener(OnButtonPressed);
+        }
+
+        /// <summary>
+        /// Handles when record button is clicked
+        /// </summary>
+        private void OnButtonPressed()
+        {
+            // click in button 
+            buttonMenu.ClickButton();
+
+            // TODO: after 3 seconds, stop recording
+
+            if (!microphoneRecord.IsRecording)
+            {
+                stream.StartStream();
+                microphoneRecord.StartRecord();
+            }
+            else
+                microphoneRecord.StopRecord();
+
+            buttonText.text = microphoneRecord.IsRecording ? stopRecordingText : recordText;
+        }
+
+        private void OnRecordStop(AudioChunk recordedAudio)
+        {
+            buttonText.text = recordText;
+        }
+
+        private void OnResult(string result)
+        {
+            text.text = result;
+
+            //handle result
+            if (result.ToLower().Contains("forward"))
+            {
+                Debug.Log("Moving object forward...");
+                ARInteractionsManager.Instance.MoveObjectForward();
+            }
+
+        }
+
+        private void OnSegmentUpdated(WhisperResult segment)
+        {
+            print($"Segment updated: {segment.Result}");
+        }
+
+        private void OnSegmentFinished(WhisperResult segment)
+        {
+            print($"Segment finished: {segment.Result}");
+        }
+
+        private void OnFinished(string finalResult)
+        {
+            print("Stream finished!");
         }
 
         #endregion
