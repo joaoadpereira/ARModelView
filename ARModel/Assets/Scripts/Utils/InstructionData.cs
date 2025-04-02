c1ed993cc7673ffe7efffb9c3179477ae4c8a89d
Joao Pereira <joaoadelino.pereira@gmail.com>
2025-04-02 11:34:22 +0100

    fix(unity): Fix instructions

--- ARModel/Assets/Scripts/Utils/InstructionData.cs
+++ ARModel/Assets/Scripts/Utils/InstructionData.cs
@@ -15,10 +15,16 @@ namespace Utils {
         private string[] instructions = new string[]
         {
             "Point your camera to a surface to detect an AR plane.",
-            "When an AR plane is detected, click on it to add objects.",
-            "Click on top of an added object to interact with it.",
-            "Use your fingers to move, rotate and scale the object.",
-            "If you need to open these instructions again, click in the help button."
+            "An AR plane can have different colors: Orange for vertical plane, Blue for floor plane and " +
+            "purple for other horizontal surfaces.",
+            "When an AR plane is detected, click on it to add an Object.",
+            "Click on top of an added object and use your fingers to move, rotate and scale it.",
+            "Use the object menu to delete it and to record a voice command like \"rotate\", \"forward\" and \"scale\".",
+            "You can add more objects by clicking in AR planes. Be aware that the objects you have, " +
+            "the more performance constraints will exist.",
+            "Use the menu bar to to toggle AR Notes, see the AR planes, select other object to add, " +
+            "activate physics engine between the objects.",
+            "If you need to open these instructions again, click in the instructions button."
         };
 
         private int currentInstructionIndex = 0;
